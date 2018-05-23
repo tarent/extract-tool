e37a78505cda558a8f5f40af8c17b3a1e38906ef
mirabilos <t.glaser@tarent.de>
2018-05-23 19:28:31 +0200

    add a workable example

---
+++ src/dist/example.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+exec java \
+    -Djdbc.driver=org.postgresql.Driver \
+    -Djdbc.url=jdbc:postgresql://localhost:5432/hellophpworld \
+    -Djdbc.username=hellophpworld \
+    -Djdbc.password=somepassword \
+    -jar extract-tool-*-cli.jar \
+    -c /dev/null \
+    -J ~/.m2/repository/org/postgresql/postgresql/42.2.1/postgresql-42.2.1.jar \
+    example.jsn
