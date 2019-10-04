1813488f92e907740ad83d17a040b0000f0fa780
mirabilos <t.glaser@tarent.de>
2019-10-04 17:21:14 +0200

    allow passing different flags more easily

--- src/dist/example.sh
+++ src/dist/example.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 exec java \
-    -Djdbc.driver=org.postgresql.Driver \
-    -Djdbc.url=jdbc:postgresql://localhost:5432/hellophpworld \
-    -Djdbc.username=hellophpworld \
-    -Djdbc.password=bI6mtPBxVjEJ \
+    -Djdbc.driver="${db:-org.postgresql.Driver}" \
+    -Djdbc.url="${dburl:-jdbc:postgresql://localhost:5432/hellophpworld}" \
+    -Djdbc.username="${dbuser:-hellophpworld}" \
+    -Djdbc.password="${dbpass:-bI6mtPBxVjEJ}" \
     -jar extract-tool-*-cli.jar \
     -c /dev/null \
     -J ~/.m2/repository/org/postgresql/postgresql/42.2.8/postgresql-42.2.8.jar \
-    "$(dirname "$0")/example.jsn"
+    "${1:-$(dirname "$0")/example.jsn}"
