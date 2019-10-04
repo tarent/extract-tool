83c95920f92226fd032b6fd3f5ba796d21422037
mirabilos <t.glaser@tarent.de>
2019-10-04 17:17:47 +0200

    update PostgreSQL CLI example

--- src/dist/example.sh
+++ src/dist/example.sh
@@ -3,8 +3,8 @@ exec java \
     -Djdbc.driver=org.postgresql.Driver \
     -Djdbc.url=jdbc:postgresql://localhost:5432/hellophpworld \
     -Djdbc.username=hellophpworld \
-    -Djdbc.password=somepassword \
+    -Djdbc.password=bI6mtPBxVjEJ \
     -jar extract-tool-*-cli.jar \
     -c /dev/null \
-    -J ~/.m2/repository/org/postgresql/postgresql/42.2.5/postgresql-42.2.5.jar \
-    example.jsn
+    -J ~/.m2/repository/org/postgresql/postgresql/42.2.8/postgresql-42.2.8.jar \
+    "$(dirname "$0")/example.jsn"
