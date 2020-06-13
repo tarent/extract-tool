cdd8eba95901910dc6b0c48ef0613659cfb8e577
mirabilos <t.glaser@tarent.de>
2020-06-13 04:25:45 +0200

    reformat

--- src/main/java/de/tarent/extract/DateExtractor.java
+++ src/main/java/de/tarent/extract/DateExtractor.java
@@ -32,11 +32,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DateExtractor implements ResultSetValueExtractor {
-
     @Override
     public Object extractValue(final ResultSet rs, final int col) throws SQLException {
         final Date date = rs.getDate(col + 1);
         return date == null ? null : date.getTime();
     }
-
 }
--- src/main/java/de/tarent/extract/DefaultExtractor.java
+++ src/main/java/de/tarent/extract/DefaultExtractor.java
@@ -31,11 +31,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DefaultExtractor implements ResultSetValueExtractor {
-
-	@Override
-	public Object extractValue(ResultSet rs, int col) throws SQLException {
-		final String string = rs.getString(col + 1);
-		return string==null?null:string.trim();
-	}
-
+    @Override
+    public Object extractValue(ResultSet rs, int col) throws SQLException {
+        final String string = rs.getString(col + 1);
+        return string == null ? null : string.trim();
+    }
 }
