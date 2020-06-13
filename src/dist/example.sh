d4eeeb1a6abd8bc36485d84c31487d527796956e
mirabilos <t.glaser@tarent.de>
2020-06-13 03:53:47 +0200

    ensure we have a PostgreSQL JDBC to test with (and bump version)

--- src/dist/example.sh
+++ src/dist/example.sh
@@ -3,8 +3,8 @@ exec java \
     -Djdbc.driver="${db:-org.postgresql.Driver}" \
     -Djdbc.url="${dburl:-jdbc:postgresql://localhost:5432/hellophpworld}" \
     -Djdbc.username="${dbuser:-hellophpworld}" \
-    -Djdbc.password="${dbpass:-bI6mtPBxVjEJ}" \
+    -Djdbc.password="${dbpass:-P68ntEvJQbhI}" \
     -jar extract-tool-*-cli.jar \
     -c /dev/null \
-    -J ~/.m2/repository/org/postgresql/postgresql/42.2.8/postgresql-42.2.8.jar \
+    -J ~/.m2/repository/org/postgresql/postgresql/42.2.14/postgresql-42.2.14.jar \
     "${1:-$(dirname "$0")/example.jsn}"
